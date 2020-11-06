2638ad4749be19f558210e9db8ac684ce33bab8b
ChrisLennoxS <36491436+ChrisLennoxS@users.noreply.github.com>
2020-11-05 21:47:14 -0800

    commentSkeleton

---
+++ Scott_Lennox_Lab2.py
@@ -0,0 +1,27 @@
+# ------------------------------------------
+# CECS 378 Lab 2
+# Name: Lennox Scott
+# I.D. #: 017071819
+# Start Date: 09-02-2020
+# End Date: 11-13-2020
+# Malware
+# ------------------------------------------
+
+from enum import enum
+
+
+#  Enum for attributes to be changed. The values will hold the offsets
+
+class Attribute(Enum):
+    keys = 1
+    skullKeys = 1
+    gems = 1
+    blackBadge = 1
+    magicCarpet = 1
+    magicAxes = 1
+
+
+#  List to hold current characters in the game
+
+#  Gui to modify specific attributes
+
